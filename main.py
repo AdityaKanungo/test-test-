7c5422c1bcfef8408770b9243e69512719b2f6d5
Aditya Kanungo <aditya.kanungo@gmail.com>
2025-04-20 23:12:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+from PDFNetPython3 import PDFDoc, Annot
 
+# Load the PDF
+doc = PDFDoc("example.pdf")
+doc.InitSecurityHandler()
+
+# Loop through annotations
+for page in doc.GetPageIterator():
+    for annot in page.GetAnnots():
+        if annot.GetType() == Annot.e_StrikeOut:
+            annot.Erase()
+doc.Save("cleaned.pdf", 0)
